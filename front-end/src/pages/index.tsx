0ab0da93dbdd66095171f498d5907c5df0063eba
José Guillardi <63321040+JoseGu1llardi@users.noreply.github.com>
2022-12-26 15:07:06 +0000

    ...

---
+++ front-end/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Barber Pro - Your complete system</title>
+      </Head>
+      <h1>Project test</h1>
+    </>
+  )
+}
